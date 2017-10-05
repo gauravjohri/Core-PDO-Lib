1533935c6cf32af3d3c119e7d9c0c425e7ca23b0
gauravjohri <gaurav_johri@ymail.com>
2017-10-05 08:59:30 +0530

    Add files via upload

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ class lib
 	function __construct()
 	{
 		$this->db=new PDO("mysql:host=localhost;dbname=test","root","");
+		$this->base_url="http://localhost/chat/";
+		$this->current_url=(isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";;
 	}
 	function insert($data,$table)
 	{
@@ -49,6 +51,14 @@ class lib
 		return $rdata;
 	}
 	
+	function query($sql)
+	{
+		$sql=$sql;
+		$row=$this->db->prepare($sql);
+		$rw=$row->execute();
+		return $row;
+	}
+	
 	
 	function getDataByID($table,$id)
 	{
@@ -86,6 +96,81 @@ class lib
 		}
 		
 
+	}
+	
+	function editfunction($table,$id)
+	{
+		if(isset($_POST['edt_'.$table]))
+		{
+			//print_r($_POST[$table]);
+			$this->update($_POST[$table],$table,$id);
+		}
+		echo "<table><form method='post'>";
+		$row=$this->getDataByID($table,$id);
+		unset($row['id']);
+		foreach($row as $key=>$val)
+		{
+			$r=$table."[".$key."]";
+			echo "<tr><td style='text-transform:capitalize;'><label>".str_replace('_','',$key)."</label></td>";
+			echo "<td><input type='text' name='".$r."' value='".$val."' /></td></tr>";
+		}
+		echo "<tr><td></td><td><input type='submit' name='edt_".$table."' value='Update' /></td></tr>";;
+		echo "</form></table>";
+		
+	}
+	
+	function showfunction($table)
+	{
+		echo "<table>";
+		$q = $this->query("DESCRIBE $table");
+		$col = $q->fetchAll(PDO::FETCH_COLUMN);
+		
+		foreach($col as $val)
+		{
+			echo "<th>".$val."</th>";
+		}
+		echo "<th>Edit</th>";
+		echo "<th>Delete</th>";
+		echo "</tr>";
+		$rows=$this->getData($table);
+		//echo "<pre>";print_r($rows);
+		foreach($rows as $row)
+		{
+			echo "<tr>";
+			foreach($col as $cols)
+			{
+				//echo $cols."<br />";
+				echo "<td>".$row[$cols]."</td>";
+			}
+				echo "<td><a href='#'>Edit</a></td>";
+				echo "<td><a href='#'>Delete</a></td>";
+			echo "</tr>";
+		}
+		
+		echo "</table>";
+	}
+	
+	function addfunction($table)
+	{
+		if(isset($_POST['add_'.$table]))
+		{
+			//print_r($_POST[$table]);
+			$this->insert($_POST[$table],$table);
+		}
+		echo "<table><form method='post'>";
+		$q = $this->query("DESCRIBE $table");
+		$row = $q->fetchAll(PDO::FETCH_COLUMN);
+		//print_r($row);
+		unset($row[0]);
+		foreach($row as $key)
+		{
+			$r=$table."[".$key."]";
+			echo "<tr><td style='text-transform:capitalize;'><label>".str_replace('_','',$key)."</label></td>";
+			echo "<td><input type='text' name='".$r."' /></td></tr>";
+		}
+		echo "<tr><td></td><td><input type='submit' name='add_".$table."' value='Add' /></td></tr>";;
+		echo "</form></table>";
+		
 	}
 	
 	function drawForm($cls,$data)
